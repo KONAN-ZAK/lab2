2450a92b500abbe62aa4999e77ace57883b7a7da
kenan <94470969+Kenan-Alzakout@users.noreply.github.com>
2022-04-19 01:23:14 +0300

    cpp

---
+++ OOP2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+for (int row = 1; row < 6; row++) { 
+
+  for (int col = 1 ; row>= col ; col++) {
+    cout << col <<"\t" ;
+  }
+  cout << endl; 
+  }
+  
+
+  return 0;
+  
+}
